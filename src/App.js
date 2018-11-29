a812112878401fd2bfd3abdfdb0b489e06ff4a47
Viet Anh Tran <viet.anhtran@myob.com>
2018-11-29 17:01:08 +1300

    Make call to backend and display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      welcomeMessage: ""
+    };
+  }
+
+  async componentDidMount() {
+    const result = await axios.get("http://localhost:8000");
+    this.setState({ welcomeMessage: result.data });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            VIETTTTTTTTT
-          </p>
+          <p>{this.state.welcomeMessage}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
